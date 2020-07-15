a323fa59369031175dc70fdd8d2272f115ecc935
logesh-selvakumar <67770988+logesh-selvakumar@users.noreply.github.com>
2020-07-15 13:01:16 -0600

    added changes

--- main.js
+++ main.js
@@ -1,62 +1,24 @@
 document.getElementById('random').addEventListener('click', getRandomName);
 document.getElementById('all').addEventListener('click', getAllNames);
 
-let nameArray = ["The Joker", "The Genius", "The Cutie", "Cheesy", "Sweaty", "The Flash"];
+let nameArray;
+fetch("words.txt").then((rawData) => rawData.text()).then((data) => nameArray = data.split("\r\n"));
 
-let randomArray = nameArray[Math.floor(Math.random() * nameArray.length)];
-
-function firstNameValue()
+function getAllNames()
 {
+    document.getElementById('name').innerHTML = '';
     let inputValue = document.getElementById('firstNameInput').value;
-    document.getElementById('fname1').innerHTML = inputValue;
-    document.getElementById('fname2').innerHTML = inputValue;
-    document.getElementById('fname3').innerHTML = inputValue;
-    document.getElementById('fname4').innerHTML = inputValue;
-    document.getElementById('fname5').innerHTML = inputValue;
-    document.getElementById('fname6').innerHTML = inputValue; 
-}
-
-function lastNameValue()
-{
-    let inputValue = document.getElementById('lastNameInput').value;
-    document.getElementById('lname1').innerHTML = inputValue;
-    document.getElementById('lname2').innerHTML = inputValue;
-    document.getElementById('lname3').innerHTML = inputValue;
-    document.getElementById('lname4').innerHTML = inputValue;
-    document.getElementById('lname5').innerHTML = inputValue;
-    document.getElementById('lname6').innerHTML = inputValue; 
-    
+    let inputVal = document.getElementById('lastNameInput').value;
+    for (i=0; i<nameArray.length; i++)
+    {
+        document.getElementById('name').innerHTML += inputValue + ` "${nameArray[i]}" `+  inputVal + '<br>';
+    }   
 }
 
 function getRandomName()
 {
     let inputValue = document.getElementById('firstNameInput').value;
-    document.getElementById('fname1').innerHTML = inputValue;
-    document.getElementById('name1').innerHTML = `"${randomArray}"`;
     let inputVal = document.getElementById('lastNameInput').value;
-    document.getElementById('lname1').innerHTML = inputVal;
+    let randomArray = nameArray[Math.floor(Math.random() * nameArray.length)]; 
+    document.getElementById('name').innerHTML = inputValue + ` "${randomArray}" ` + inputVal;
 }
-
-function getAllNames()
-{
-    firstNameValue();
-    document.getElementById('name1').innerHTML = `"${nameArray[0]}"`;
-    document.getElementById('name2').innerHTML = `"${nameArray[1]}"`;
-    document.getElementById('name3').innerHTML = `"${nameArray[2]}"`;
-    document.getElementById('name4').innerHTML = `"${nameArray[3]}"`;
-    document.getElementById('name5').innerHTML = `"${nameArray[4]}"`;
-    document.getElementById('name6').innerHTML = `"${nameArray[5]}"`;
-    lastNameValue();
-}
-
-
-
-
-
-
-
-
-
-
-
-
